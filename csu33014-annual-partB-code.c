79c972fafc9a881fd1347da45da38a4d159b502b
rossmccrann <rossmccrann@users.noreply.github.com>
2020-05-14 20:03:09 +0100

    init

--- csu33014-annual-partB-code.c
+++ csu33014-annual-partB-code.c
@@ -125,7 +125,7 @@ int* reachable;
 reachable = malloc(sizeof(int)*total_people);
 int steps = k;
 
-#pragma omp parallel for collapse(3);
+#pragma omp parallel for collapse(2);
 for(int i = 0 ; i < total_people; i++){
   reachable[i] = 0;
 }
