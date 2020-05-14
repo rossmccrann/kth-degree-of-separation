9642042f994ffe2f95bb1b695b8859979ec1c9aa
rossmccrann <rossmccrann@users.noreply.github.com>
2020-05-14 21:30:49 +0100

    init

--- csu33014-annual-partB-code.c
+++ csu33014-annual-partB-code.c
@@ -49,19 +49,23 @@ void find_reachable_recursive3(struct person * current, int steps_remaining, int
 
   if(steps_remaining > 0)
   {
-    int num_known = person_get_num_known(current);
 
+
+    int num_known = person_get_num_known(current);
+  #pragma omp task
+
+{
     for (int i = 0; i < num_known; i++)
     {
       struct person* acquaintance = person_get_acquaintance(current, i);
 
      if(reachable[person_get_index(acquaintance)] > steps-steps_remaining || reachable[person_get_index(acquaintance)]==  0){
-  #pragma omp task
 {
 
        find_reachable_recursive3(acquaintance, steps_remaining-1, reachable, steps);
-}
+
     }
+  }
 
 }
 
