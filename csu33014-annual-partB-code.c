cc1ab2cf9ac4d38be64caea57679c1c3cb4be6eb
rossmccrann <rossmccrann@users.noreply.github.com>
2020-05-14 20:54:46 +0100

    init

--- csu33014-annual-partB-code.c
+++ csu33014-annual-partB-code.c
@@ -53,7 +53,7 @@ void find_reachable_recursive3(struct person * current, int steps_remaining, int
     {
       struct person* acquaintance = person_get_acquaintance(current, i);
 
-     if(reachable[person_get_index(acquaintance)] > steps-steps_remaining || reachable[person_get_index(acquaintance)]==  0){
+     if(reachable[person_get_index(acquaintance)] > k - steps_remaining || reachable[person_get_index(acquaintance)]==  0){
 
        find_reachable_recursive3(acquaintance, steps_remaining-1, reachable);
 
