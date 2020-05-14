f16cae57b00822a3fe4cbbd69d58f57681fd5cef
rossmccrann <rossmccrann@users.noreply.github.com>
2020-05-15 00:06:00 +0100

    init

--- csu33014-annual-partB-code.c
+++ csu33014-annual-partB-code.c
@@ -3,6 +3,45 @@
 // Part B of a two-part assignment
 //
 // Please write your solution in this file
+
+/*      
+
+
+
+*** Optimised number within k degrees using Recursion *** 
+
+
+
+I replaced the Boolean reachable array (reachable[person_get_index(current)] = true;)
+with an array of ints (int   reachable[i] = 0;) as the integer value can be utilised 
+to optimise the code. Each int reacable[i] represents the distance from the intial 
+person ( person * start) to the nth aquaintance in the array.
+If you are at the current person '(person_get_acquaintance(current, i)' it checks the 
+distance that it is from starting to the distance of the potential aquaintance that it
+wants to get to. If the aquaintances distance is greater than the current distance then 
+that aquaintace has already been visited as all of the initial values are set to zero. 
+Hence if the distance is greater than the current value then it has been visited.
+This is represented with an if statement around the recursive call 
+
+*** Parallelisation using OpenMP ***
+
+The best way to parrallelise the code is to use the OpenMP parallel pragma.The block 
+after the #pragma omp parallel is executed by a group of threads. The task is split up
+and ran simultaneously on multiple processors with different inputs so that the result 
+is obtained faster.
+
+I use 'omp_set_dynamic(0);' to explicitly disable dynamic teams and then use 
+'omp_set_num_threads(4);' to override the value of the environmental variable that controls
+the upper limit of the size of the thread team that is created for all of the parrallel 
+segments in the code.
+
+The OpenMP Single ensures that the recursive function (parallel_reachable_recursive) will 
+only be executed once by single thread. The OpenMp pragma nowait turn off the barrier on 
+the sinle however there is already a barrier on the parallel.
+
+
+*/
+
 #include <pthread.h> 
 
 #include <stdio.h>
@@ -26,14 +65,15 @@ void find_reachable_recursive(struct person * current, int steps_remaining,
 }
 
 void lr_reachable_recursive(struct person * current, int steps_remaining, int * reachable, int distance){
-  reachable[person_get_index(current)] = distance - steps_remaining;
+  int curr = distance - steps_remaining;
+  reachable[person_get_index(current)] = curr;
   if(steps_remaining > 0)
   {
     int num_known = person_get_num_known(current);
     for (int i = 0; i < num_known; i++)
     {
       struct person* acquaintance = person_get_acquaintance(current, i);
-      if(reachable[person_get_index(acquaintance)] ==  0 || distance-steps_remaining < reachable[person_get_index(acquaintance)]) {
+      if(reachable[person_get_index(acquaintance)] ==  0 || curr < reachable[person_get_index(acquaintance)]) {
        lr_reachable_recursive(acquaintance, steps_remaining-1, reachable, distance);
     }
 }
@@ -42,7 +82,8 @@ void lr_reachable_recursive(struct person * current, int steps_remaining, int * 
 }
 
 void parallel_reachable_recursive(struct person * current, int steps_remaining, int * reachable, int distance){
-  reachable[person_get_index(current)] = distance - steps_remaining;
+  int curr = distance - steps_remaining;
+  reachable[person_get_index(current)] = curr;
 
   if(steps_remaining > 0)
   {
@@ -55,7 +96,7 @@ void parallel_reachable_recursive(struct person * current, int steps_remaining, 
       struct person* acquaintance = person_get_acquaintance(current, i);
   
 
-      if(reachable[person_get_index(acquaintance)] ==  0 || distance-steps_remaining < reachable[person_get_index(acquaintance)]) {
+      if(reachable[person_get_index(acquaintance)] ==  0 || curr < reachable[person_get_index(acquaintance)]) {
     
        parallel_reachable_recursive(acquaintance, steps_remaining-1, reachable, distance);
 }
@@ -151,9 +192,9 @@ for(int i =0; i < total_people; i++){
 return count;
 }
 
-/*
+/*      
 
-*** Optimised number within k degrees using Recursion ***
+*** Optimised number within k degrees using Recursion *** 
 
 I replaced the Boolean reachable array (reachable[person_get_index(current)] = true;)
 with an array of ints (int   reachable[i] = 0;) as the integer value can be utilised 
@@ -182,9 +223,6 @@ segments in the code.
 The OpenMP Single ensures that the recursive function (parallel_reachable_recursive) will 
 only be executed once by single thread. The OpenMp pragma nowait turn off the barrier on 
 the sinle however there is already a barrier on the parallel.
-
-
-
 
 */
 
